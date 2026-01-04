2e2573963fccfe6259fa3b3a9abec9439ba3625d
liar2357 <sushikora12@gmail.com>
2026-01-04 22:29:39 +0900

    change conig @ nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,6 +46,29 @@ vim.opt.clipboard = "unnamedplus"
 local is_vscode = (vim.g.vscode ~= nil)
 
 if not is_vscode then
+  vim.api.nvim_create_autocmd('QuitPre', {
+    callback = function()
+      -- 現在のウィンドウ番号を取得
+      local current_win = vim.api.nvim_get_current_win()
+      -- すべてのウィンドウをループして調べる
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        -- カレント以外を調査
+        if win ~= current_win then
+          local buf = vim.api.nvim_win_get_buf(win)
+          -- buftypeが空文字（通常のバッファ）があればループ終了
+          if vim.bo[buf].buftype == '' then
+            return
+          end
+        end
+      end
+      -- ここまで来たらカレント以外がすべて特殊ウィンドウということなので
+      -- カレント以外をすべて閉じる
+      vim.cmd.only({ bang = true })
+      -- この後、ウィンドウ1つの状態でquitが実行されるので、Vimが終了する
+    end,
+    desc = 'Close all special buffers and quit Neovim',
+  })
+
   require('config.lazy')
 
   --require("color.ts_highlight_pastel").setup()
