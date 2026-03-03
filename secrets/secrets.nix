4d41cf3f5de4baf9f4b9202f810c7828de3f22d9
liar2357 <sushikora12@gmail.com>
2026-03-04 02:20:28 +0900

    add wireguard config @nix

--- secrets/secrets.nix
+++
@@ -1,9 +0,0 @@
-{
-  "wg-server-pubkey.age".publicKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKI4HWgiJZHoI6AaFE64vxdAK94RzUkhbysBUkXmRKrZ sushikora12@gmail.com"
-  ];
-
-  "wg-endpoint.age".publicKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKI4HWgiJZHoI6AaFE64vxdAK94RzUkhbysBUkXmRKrZ sushikora12@gmail.com"
-  ];
-}
