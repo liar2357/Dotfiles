a8af5e6004fb9df0af6c243a1d3be3cf6ecd9710
liar2357 <sushikora12@gmail.com>
2026-03-04 01:08:02 +0900

    wip

--- flake.nix
+++ flake.nix
@@ -3,52 +3,66 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05"; 
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    agenix.url = "github:ryantm/agenix";
     hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # 自分の dotfiles はここがベース flake
-    # このファイル自体が repo のルート flake.nix
-    self = { };
-
+    nix-hazkey.url = "github:aster-void/nix-hazkey";
+    self = {};
   };
 
-  outputs = { nixpkgs, hyprland, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, hyprland, ... } :
   let
     system = "x86_64-linux";
-    pkgs   = nixpkgs.legacyPackages.${system};
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations.NCP-2602 = nixpkgs.lib.nixosSystem {
       inherit system;
 
-      modules = [
-        ./nixos/configuration.nix                      # 自分の config
-        home-manager.nixosModules.home-manager         # Home-Manager module
+      # ここで specialArgs を設定
+      specialArgs = { inherit inputs; };
 
+      modules = [
+	./nixos/hardware-configuration.nix 
+        ./nixos/configuration.nix
+
+	{
+          _module.args = {
+            inherit inputs;
+	  };
+	}
+
+        # ① Home-Manager を NixOS モジュールとして読み込ませる
+        home-manager.nixosModules.home-manager
+        {
+          # ② ここで Home-Manager の設定をひとまとまりで渡す
+          home-manager.useGlobalPkgs  = true;
+          home-manager.useUserPackages = true;
+
+          # ↓ ここが重要
+	  home-manager.extraSpecialArgs = { inherit inputs; };
+
+          # ③ ユーザー側の home.nix をここで読み込む
+          home-manager.users."raia" = import ./home-manager/home.nix;
+        }
+
+        # ④ Hyprland のモジュール
         hyprland.nixosModules.default
         {
-          programs.hyprland = {
-            enable = true;
-            xwayland.enable = true;
-          };
+          programs.hyprland.enable = true;
+          programs.hyprland.xwayland.enable = true;
         }
       ];
 
-      # Home-Manager のユーザー設定を inject
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.users."raia" = import ./home-manager/home.nix;
-
+      # optional: ここに system-level options を書ける
     };
-    
-    # --- Home-Manager 単独でも switch 可能 ---
+
+    # (オプション) Home-Manager を standalone でも動かせるようにする
     homeConfigurations."raia" =
       home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
@@ -56,5 +70,3 @@
       };
   };
 }
-
-
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,78 +1,83 @@
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
+  home.username = "raia";
+  home.homeDirectory = "/home/raia";
 
+  # ← 必ず書く
+  home.stateVersion = "25.05";
+  
   # --- ~/.config 配下 ---
 
   xdg.configFile."fuzzel" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/fuzzel";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/fuzzel";
     recursive = true;
   };
 
   xdg.configFile."kitty" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/kitty";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/kitty";
     recursive = true;
   };
 
   xdg.configFile."mako" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/mako";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/mako";
     recursive = true;
   };
 
   xdg.configFile."nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/nvim";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/nvim";
     recursive = true;
   };
 
   xdg.configFile."nvim-lite" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/nvim-lite";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/nvim-lite";
     recursive = true;
   };
 
   xdg.configFile."rofi" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/rofi";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/rofi";
     recursive = true;
   };
 
   xdg.configFile."snippet-source" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/snippet-source";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/snippet-source";
     recursive = true;
   };
 
   xdg.configFile."waybar" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/waybar";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/waybar";
     recursive = true;
   };
 
   xdg.configFile."wofi" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/wofi";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/wofi";
     recursive = true;
   };
 
   # --- hypr ---
   xdg.configFile."hypr/common" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/hypr/common";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/hypr/common";
     recursive = true;
   };
 
   xdg.configFile."hypr/scripts" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/hypr/scripts";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/hypr/scripts";
     recursive = true;
   };
 
   xdg.configFile."hypr/hyprland.conf" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/config/hypr/NCP-2602.conf";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/config/hypr/NCP-2602.conf";
     recursive = true;
   };
 
 
   # --- ~/ 配下 ---
   home.file.".zshrc" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/home/.zshrc";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/home/.zshrc";
   };
 
   home.file."zsh" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${self}/home/zsh";
+    source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/home/zsh";
     recursive = true;
   };
 
