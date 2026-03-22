5193a4bcc408a05ba807d564ea1d50f209a2e03f
liar2357 <sushikora12@gmail.com>
2026-03-22 19:01:32 +0900

    optimyze for ADS-2509

--- others/zsh-init.sh
+++ others/zsh-init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
