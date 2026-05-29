7bc06685d77be0d1c17096120ef20ef71afafb1e
liar2357 <sushikora12@gmail.com>
2026-05-29 10:06:25 +0900

    fix config @nvim

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    -- NOTE: master を使うのが安定、main でも動くけど README では推奨されている
     branch = "master",
     lazy = false,
-    --build = ":TSUpdate",
+    build = ":TSUpdate",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
