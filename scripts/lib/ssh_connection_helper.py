af5677252d7078037baefef74fd4a0a54fde3243
liar2357 <sushikora12@gmail.com>
2026-09-11 08:14:45 +0900

    fix ssh helper

--- scripts/lib/ssh_connection_helper.py
+++ scripts/lib/ssh_connection_helper.py
@@ -9,19 +9,27 @@ hosts = [
 ]
 
 
-def exec_ssh_connection(hostname: str):
-    subprocess.call(["ssh", hostname])
+def exec_ssh_connection(hostname: str, isTermuxMode: bool):
+    if isTermuxMode:
+        subprocess.call(["ssh-wraper", hostname])
+
+    else:
+        subprocess.call(["ssh", hostname])
 
 
-def exec_mosh_connection(hostname: str):
-    subprocess.call(["mosh", hostname])
+def exec_mosh_connection(hostname: str, isTermuxMode: bool):
+    if isTermuxMode:
+        subprocess.call(["mosh-wraper", hostname])
+
+    else:
+        subprocess.call(["mosh", hostname])
 
 
-def ssh_connection_helper():
+def ssh_connection_helper(isTermuxMode: bool):
     print("===SSH HOSTS===")
 
     for i, host in enumerate(hosts):
-        print(f"{i}: {host[0]}")
+        print(f"{i+1}: {host[0]}")
 
     num: int
 
@@ -32,19 +40,19 @@ def ssh_connection_helper():
             try:
                 num = int(num_str)
 
-                if 0 <= num < len(hosts):
+                if 1 <= num < len(hosts):
                     break
                 else:
-                    print(f"Avalable is 0~{len(hosts)-1}")
+                    print(f"Avalable is 1~{len(hosts)-1}")
             except ValueError:
                 print("Input Is Must Be Integer")
                 continue
 
-    if hosts[num][1]:
+    if hosts[num - 1][1]:
         yn = input("You Can Use Mosh for SSH Connection. (Y/n) : ")
         if yn == "n" or yn == "N":
-            exec_ssh_connection(hosts[num][0])
+            exec_ssh_connection(hosts[num - 1][0], isTermuxMode)
         else:
-            exec_mosh_connection(hosts[num][0])
+            exec_mosh_connection(hosts[num - 1][0], isTermuxMode)
     else:
-        exec_ssh_connection(hosts[num][0])
+        exec_ssh_connection(hosts[num - 1][0], isTermuxMode)
